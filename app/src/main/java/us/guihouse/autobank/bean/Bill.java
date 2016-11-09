7faf698ba4e7bcefc3174ae2ba273871a0c0bb5b
Valmir Jr <valmirmassoni@gmail.com>
2016-11-09 16:33:47 -0200

    Bill Models

--- app/src/main/java/us/guihouse/autobank/bean/Bill.java
+++ app/src/main/java/us/guihouse/autobank/bean/Bill.java
@@ -1,11 +1,46 @@
 package us.guihouse.autobank.bean;
 
+import java.util.Date;
+
 /**
  * Created by aluno on 18/10/16.
  */
 
-public class Bill {
+public abstract class Bill {
+    private Long id;
+    private int month;
+    private int year;
+    private Date paymentDeadline;
 
-    
+    public Long getId() {
+        return id;
+    }
 
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Date getPaymentDeadline() {
+        return paymentDeadline;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPaymentDeadline(Date paymentDeadline) {
+        this.paymentDeadline = paymentDeadline;
+    }
 }
---
+++ app/src/main/java/us/guihouse/autobank/bean/ClosedBill.java
@@ -0,0 +1,49 @@
+package us.guihouse.autobank.bean;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by valmir.massoni on 09/11/2016.
+ */
+
+public class ClosedBill extends Bill {
+    private BigDecimal totalValue;
+    private BigDecimal minValue;
+
+    public BigDecimal getTotalValue() {
+        return totalValue;
+    }
+
+    public BigDecimal getMinValue() {
+        return minValue;
+    }
+
+    public void setTotalValue(BigDecimal totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public void setMinValue(BigDecimal minValue) {
+        this.minValue = minValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ClosedBill that = (ClosedBill) o;
+
+        if (!totalValue.equals(that.totalValue)) return false;
+        return minValue.equals(that.minValue);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + totalValue.hashCode();
+        result = 31 * result + minValue.hashCode();
+        return result;
+    }
+}
