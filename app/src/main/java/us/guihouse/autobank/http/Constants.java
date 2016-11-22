74f6a2809ac215b23d630c4a7ed61f4a0bf360f8
aluno <guilherme_otran@hotmail.com>
2016-11-22 21:51:43 -0200

    Constantes

--- app/src/main/java/us/guihouse/autobank/http/Constants.java
+++ app/src/main/java/us/guihouse/autobank/http/Constants.java
@@ -5,11 +5,11 @@ package us.guihouse.autobank.http;
  */
 
 public class Constants {
-    public static final String BASE_URL = "http://10.8.1.50:8080/AutoBank_Server_war_exploded";
-    public static final String LOGIN_URL = BASE_URL + "/LoginServlet";
-    public static final String BILLS_URL = BASE_URL + "/BillsServlet";
-    public static final String FINANTIAL_STATEMENTS_URL  = BASE_URL + "/FinantialStatements";
-    public static final String CARD_URL = BASE_URL + "/Card";
+    public static final String BASE_URL = "http://192.168.218.253:8080/AutoBank/api/";
+    public static final String LOGIN_URL = BASE_URL + "login";
+    public static final String BILLS_URL = BASE_URL + "bill";
+    public static final String FINANTIAL_STATEMENTS_URL  = BASE_URL + "finantial-statements";
+    public static final String CARD_URL = BASE_URL + "card";
 
     public static final String SHARED_PREFS_FILE = "us.guihouse.autobank.token";
     public static final String SHARED_PREFS_TOKEN = "token";
