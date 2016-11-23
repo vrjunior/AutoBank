7b115bd479c51a2e8cbd939f8ce0b31ade0c3d11
Guilherme Otranto Aulicino <guilherme_otran@hotmail.com>
2016-11-23 18:37:18 -0200

    chamada tela do cartão

--- app/src/main/java/us/guihouse/autobank/CardFragment.java
+++ app/src/main/java/us/guihouse/autobank/CardFragment.java
@@ -4,43 +4,114 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import us.guihouse.autobank.bean.Card;
+import us.guihouse.autobank.fetchers.AuthorizedFetcher;
+import us.guihouse.autobank.http.CardRequest;
 
 public class CardFragment extends Fragment {
+    private static final String SAVED_CARD = "SAVED_CARD";
+    private Card card;
 
-    public CardFragment() {
-        // Required empty public constructor
-    }
+    private ProgressBar progress;
+    private View details;
 
-    // TODO: Rename and change types and number of parameters
-    public static CardFragment newInstance(String param1, String param2) {
-        CardFragment fragment = new CardFragment();
-        return fragment;
-    }
+    private AuthorizedFetcher<CardRequest, Card> fetcher;
+    private boolean destroyed = false;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(SAVED_CARD, card);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        if (savedInstanceState != null) {
+            card = (Card) savedInstanceState.getSerializable(SAVED_CARD);
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false);
+        View inflated = inflater.inflate(R.layout.fragment_card, container, false);
+        progress = (ProgressBar) inflated.findViewById(R.id.fragment_card_progress);
+        details = inflated.findViewById(R.id.fragment_card_details);
+
+        destroyed = false;
+        fetchCardIfNeeded();
+
+        return inflated;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    public void onDestroyView() {
+        if (fetcher != null) {
+            fetcher.cancel(true);
+        }
+
+        destroyed = true;
+        super.onDestroyView();
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+    private void fetchCardIfNeeded() {
+        if (card != null) {
+            setSuccess();
+            return;
+        }
+
+        setLoading();
+
+        fetcher = new AuthorizedFetcher<>(this.getActivity(),
+                new AuthorizedFetcher.AuthorizedFetchCallback<Card>() {
+            @Override
+            public void onSuccess(Card data) {
+                card = data;
+                fetcher = null;
+                setSuccess();
+            }
+
+            @Override
+            public void onNoConnection() {
+                card = null;
+                fetcher = null;
+                setError();
+            }
+
+            @Override
+            public void onError() {
+                card = null;
+                fetcher = null;
+                setError();
+            }
+        });
+
+        fetcher.execute(new CardRequest());
     }
 
+    private void setLoading() {
+        progress.setVisibility(View.VISIBLE);
+        details.setVisibility(View.GONE);
+    }
 
+    private void setSuccess() {
+        if (destroyed) {
+            return;
+        }
+        progress.setVisibility(View.GONE);
+        details.setVisibility(View.VISIBLE);
+    }
+
+    private void setError() {
+        if (destroyed) {
+            return;
+        }
+        progress.setVisibility(View.GONE);
+        details.setVisibility(View.GONE);
+    }
 }
