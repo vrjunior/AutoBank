bdaf2193eae828e6cb86ccb82709c9d4decbffa6
Guilherme O <guilherme_otran@hotmail.com>
2016-11-24 15:48:14 -0200

    Tela do cartão

--- app/src/main/java/us/guihouse/autobank/CardFragment.java
+++ app/src/main/java/us/guihouse/autobank/CardFragment.java
@@ -3,12 +3,21 @@ package us.guihouse.autobank;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
 
 import us.guihouse.autobank.bean.Card;
 import us.guihouse.autobank.fetchers.AuthorizedFetcher;
@@ -21,8 +30,30 @@ public class CardFragment extends Fragment {
     private ProgressBar progress;
     private View details;
 
+    private TextView cardNumber;
+    private TextView emission;
+    private TextView expiration;
+    private TextView limit;
+    private TextView interestRate;
+    private TextView closingDay;
+    private TextView availableValue;
+    private TextView usedValue;
+
     private AuthorizedFetcher<CardRequest, Card> fetcher;
     private boolean destroyed = false;
+    private DateFormat dateFormat;
+    private NumberFormat numberFormat;
+    private NumberFormat percentFormat;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dateFormat = DateFormat.getDateInstance();
+
+        numberFormat = NumberFormat.getCurrencyInstance();
+        numberFormat.setCurrency(Currency.getInstance("BRL"));
+
+        percentFormat = NumberFormat.getPercentInstance(Locale.getDefault());
+    }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -42,6 +73,15 @@ public class CardFragment extends Fragment {
         View inflated = inflater.inflate(R.layout.fragment_card, container, false);
         progress = (ProgressBar) inflated.findViewById(R.id.fragment_card_progress);
         details = inflated.findViewById(R.id.fragment_card_details);
+
+        cardNumber = (TextView) inflated.findViewById(R.id.fragment_card_ending_digits);
+        emission = (TextView) inflated.findViewById(R.id.fragment_card_emission);
+        expiration = (TextView) inflated.findViewById(R.id.fragment_card_expire);
+        limit = (TextView) inflated.findViewById(R.id.fragment_card_limit);
+        interestRate = (TextView) inflated.findViewById(R.id.fragment_card_interest_rate);
+        closingDay = (TextView) inflated.findViewById(R.id.fragment_card_closing_date);
+        availableValue = (TextView) inflated.findViewById(R.id.fragment_card_available);
+        usedValue = (TextView) inflated.findViewById(R.id.fragment_card_used);
 
         destroyed = false;
         fetchCardIfNeeded();
@@ -105,6 +145,16 @@ public class CardFragment extends Fragment {
         }
         progress.setVisibility(View.GONE);
         details.setVisibility(View.VISIBLE);
+
+        cardNumber.setText(card.getCardNumber());
+        emission.setText(dateFormat.format(card.getEmission()));
+        expiration.setText(dateFormat.format(card.getExpiration()));
+
+        limit.setText(numberFormat.format(card.getLimit()));
+        interestRate.setText(percentFormat.format(card.getInterestRate()));
+        closingDay.setText(card.getClosingDay().toString());
+        availableValue.setText(numberFormat.format(card.getAvailableValue()));
+        usedValue.setText(numberFormat.format(card.getUsedValue()));
     }
 
     private void setError() {
--- app/src/main/java/us/guihouse/autobank/http/Constants.java
+++ app/src/main/java/us/guihouse/autobank/http/Constants.java
@@ -5,7 +5,7 @@ package us.guihouse.autobank.http;
  */
 
 public class Constants {
-    public static final String BASE_URL = "http://10.0.3.207:8080/AutoBank/api/";
+    public static final String BASE_URL = "http://10.0.3.107:8080/AutoBank/api/";
     public static final String LOGIN_URL = BASE_URL + "login";
     public static final String BILLS_URL = BASE_URL + "bills";
     public static final String FINANTIAL_STATEMENTS_URL  = BASE_URL + "finantial-statements";
